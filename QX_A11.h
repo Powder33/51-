663b537772956d09218bfcae1fd426febbd89eaa
Powder33 <1743307630@qq.com>
2022-12-11 17:01:44 +0800

    51

---
+++ QX_A11.h
@@ -0,0 +1,38 @@
+#ifndef _QX_A11_H
+#define _QX_A11_H
+
+
+#define MAIN_Fosc		11059200L	//定义主时钟
+#define Main_Fosc_KHZ	(MAIN_Fosc / 1000)
+
+
+/*电机驱动IO定义*/
+sbit Left_moto_pwm = P1^5; //为1 左电机使能
+sbit IN1 = P1^4; //为1 左电机正转
+sbit IN2 = P1^3; //为1 左电机反转
+sbit IN3 = P1^2; //为1 右电机反转
+sbit IN4 = P1^1; //为1 右电机正转
+sbit Right_moto_pwm = P1^0; //为1 右电机使能
+
+sbit left_led1 = P3^5;
+sbit right_led1 = P3^4;
+sbit right_led2 = P3^6;
+sbit left_led2 = P3^7;
+
+/*按键定义*/
+sbit KEY = P3^3;
+sbit beep = P2^2;//蜂鸣器
+
+#define left_motor_en		EN1 = 1	//左电机使能
+#define right_motor_en		EN2 = 1	//右电机使能
+
+
+#define left_motor_stops	IN1 = 0, IN2 = 0//左电机停止
+#define right_motor_stops	IN3 = 0, IN4 = 0//右电机停止
+
+#define left_motor_go		IN1 = 1, IN2 = 0//左电机正传
+#define left_motor_back		IN1 = 0, IN2 = 1//左电机反转
+#define right_motor_go		IN3 = 0, IN4 = 1//右电机正传
+#define right_motor_back	IN3 = 1, IN4 = 0//右电机反转
+
+#endif
---
+++ main.c
@@ -0,0 +1,207 @@
+/*********************************************************************************
+* 【编写时间】： 2020年12月12日
+* 【作    者】： 清翔电子:03
+* 【版    本】： 1.0
+* 【网    站】： http://www.QXMBOT.com/ 
+* 【淘宝店铺】： http://qxmcu.taobao.com/ (直销店)
+* 【实验平台】： QX-A11亚克力智能小车  STC89C52
+* 【外部晶振】： 11.0592mhz	
+* 【主控芯片】： STC89C52
+* 【编译环境】： Keil μVisio4
+*	版权所有，盗版必究。
+*	Copyright(C) QXMBOT
+*	All rights reserved
+***********************************************************************************
+* 【接线说明】：请参考资料内图文说明书
+* 【程序功能】：QX-A11亚克力智能小车红外循迹		   			            			    
+* 【注意事项】：避免小车撞向障碍物或小车轮子堵转
+				1、不能在自然光强烈的条件下进行试验
+				2、进行试验前必须调节好循迹探头使其能区分循迹黑线
+				3、此程序只做参考，实际运行效果需根据不同实验场地进行不同调试
+**********************************************************************************/
+//黑线贴3厘米宽，否则小车摆动很厉害
+#include <reg52.h>//51头文件
+#include <QX_A11.h>//QX_A11智能小车配置文件
+#include <intrins.h>
+unsigned char	pwm_val_left,pwm_val_right;	//中间变量，用户请勿修改。
+unsigned char 	pwm_left,pwm_right;			//定义PWM输出高电平的时间的变量。用户操作PWM的变量。
+#define		PWM_DUTY		200			//定义PWM的周期，数值为定时器0溢出周期，假如定时器溢出时间为100us，则PWM周期为20ms。
+#define		PWM_HIGH_MIN	70			//限制PWM输出的最小占空比。用户请勿修改。
+#define		PWM_HIGH_MAX	PWM_DUTY	//限制PWM输出的最大占空比。用户请勿修改。
+
+void Timer0_Init(void); //定时器0初始化
+void LoadPWM(void);//装入PWM输出值 
+void forward(unsigned char LeftSpeed,unsigned char RightSpeed);//QX_A11智能小车前进 
+void left_run(unsigned char LeftSpeed,unsigned char RightSpeed);//QX_A11智能小车左转  
+void right_run(unsigned char LeftSpeed,unsigned char RightSpeed);//QX_A11智能小车右转
+void stop(void);//QX_A11智能小车停车
+int x,y;
+void Delay400ms();
+
+void Tracking()
+{
+    char d1 = left_led1, d2 = right_led2,d3 = right_led1;
+	//为0 没有识别到黑线 为1识别到黑线
+	if (d1 == 0 && d3 == 0) {
+        forward(170, 130);
+    }
+    if (d1 == 1 && d3 == 0) {
+        if (d1 == 1 && d2 == 0) {
+            left_run(80, 160);
+        }
+        if (d2 == 1 && d1 == 0) {
+            forward(170, 130);
+        }
+        if (d1 == 1 && d2 == 0 && d3 == 1) {
+            right_run(160, 80);
+            Delay400ms();
+        }
+        if (d2 == 1 && d1 == 1) {
+            if (x <= 1) {
+                forward(170, 130);
+                Delay400ms();
+                x++;
+            }
+            else {
+                left_run(80, 160);
+                Delay400ms();
+            }
+        }
+    }
+    if (d1 == 0 && d3 == 1) {
+        if (d2 == 0 && d3 == 1) {
+            right_run(160, 80);
+        }
+        if (d2 == 1 && d3 == 0) {
+            forward(170, 130);
+    }
+        if (d2 == 1 && d3 == 1) {
+            if (y <= 1) {
+                forward(170, 130);
+            }
+            else {
+                right_run(160, 80);
+            }
+        }
+    }
+    if (d1 == 1 && d2 == 1 && d3 == 1) {
+        stop();
+    }
+}
+
+/*主函数*/     
+void main(void)
+{
+	Timer0_Init();//定时0初始化
+    x = 0,y = 0;
+	while(1)
+	{
+		Tracking();
+	}	
+}
+
+
+/*********************************************
+QX_A11智能小车前进
+入口参数：LeftSpeed，RightSpeed
+出口参数: 无
+说明：LeftSpeed，RightSpeed分别设置左右车轮转速
+**********************************************/
+void forward(unsigned char LeftSpeed,unsigned char RightSpeed)
+{
+	pwm_left = LeftSpeed,pwm_right =  RightSpeed;//设置速度
+	left_motor_go; //左电机前进
+	right_motor_go; //右电机前进
+}
+/*小车左转*/
+/*********************************************
+QX_A11智能小车左转
+入口参数：LeftSpeed，RightSpeed
+出口参数: 无
+说明：LeftSpeed，RightSpeed分别设置左右车轮转速
+**********************************************/
+void left_run(unsigned char LeftSpeed,unsigned char RightSpeed)
+{
+	pwm_left = LeftSpeed,pwm_right =  RightSpeed;//设置速度
+	left_motor_back; //左电机后退
+	right_motor_go; //右电机前进	
+}
+
+/*********************************************
+QX_A11智能小车右转
+入口参数：LeftSpeed，RightSpeed
+出口参数: 无
+说明：LeftSpeed，RightSpeed分别设置左右车轮转速
+**********************************************/
+void right_run(unsigned char LeftSpeed,unsigned char RightSpeed)
+{
+	pwm_left = LeftSpeed,pwm_right =  RightSpeed;//设置速度
+	right_motor_back;//右电机后退
+	left_motor_go;    //左电机前进
+}
+/*********************************************
+QX_A11智能小车停车
+入口参数：无
+出口参数: 无
+说明：QX_A11智能小车停车
+**********************************************/
+void stop(void)
+{
+	left_motor_stops;
+	right_motor_stops;
+}
+/*********************************************
+QX_A11智能小车PWM输出
+入口参数：无
+出口参数: 无
+说明：装载PWM输出,如果设置全局变量pwm_left,pwm_right分别配置左右输出高电平时间
+**********************************************/
+void LoadPWM(void)
+{
+	if(pwm_left > PWM_HIGH_MAX)		pwm_left = PWM_HIGH_MAX;	//如果左输出写入大于最大占空比数据，则强制为最大占空比。
+	if(pwm_left < PWM_HIGH_MIN)		pwm_left = PWM_HIGH_MIN;	//如果左输出写入小于最小占空比数据，则强制为最小占空比。
+	if(pwm_right > PWM_HIGH_MAX)	pwm_right = PWM_HIGH_MAX;	//如果右输出写入大于最大占空比数据，则强制为最大占空比。
+	if(pwm_right < PWM_HIGH_MIN)	pwm_right = PWM_HIGH_MIN;	//如果右输出写入小于最小占空比数据，则强制为最小占空比。
+	if(pwm_val_left<=pwm_left)		Left_moto_pwm = 1;  //装载左PWM输出高电平时间
+	else Left_moto_pwm = 0; 						    //装载左PWM输出低电平时间
+	if(pwm_val_left>=PWM_DUTY)		pwm_val_left = 0;	//如果左对比值大于等于最大占空比数据，则为零
+
+	if(pwm_val_right<=pwm_right)	Right_moto_pwm = 1; //装载右PWM输出高电平时间
+	else Right_moto_pwm = 0; 							//装载右PWM输出低电平时间
+	if(pwm_val_right>=PWM_DUTY)		pwm_val_right = 0;	//如果右对比值大于等于最大占空比数据，则为零
+}
+/********************* Timer0初始化************************/
+void Timer0_Init(void)
+{
+	TMOD |= 0x02;//定时器0，8位自动重装模块
+	TH0 = 164;
+	TL0 = 164;//11.0592M晶振，12T溢出时间约等于100微秒
+	TR0 = 1;//启动定时器0
+	ET0 = 1;//允许定时器0中断
+	EA	= 1;//总中断允许	
+}
+ 
+/********************* Timer0中断函数************************/
+void timer0_int (void) interrupt 1
+{
+	 pwm_val_left++;
+	 pwm_val_right++;
+	 LoadPWM();//装载PWM输出
+}	
+
+void Delay400ms()		//@11.0592MHz
+{
+	unsigned char i, j, k;
+
+	_nop_();
+	i = 3;
+	j = 206;
+	k = 43;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
+}
